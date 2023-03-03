7d17167d14aef0a028f69850a619a454f65cbb6d
lburk2 <landonburk@me.com>
2023-03-03 09:46:37 -0600

    BIOS_HWI_IDL project creation

---
+++ programs/BIOS_HWI_IDL/Debug/main.pp
@@ -0,0 +1,165 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/std.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/tistdtypes.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/log.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/_log.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/trc.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/clk.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/tsk.h
+main.obj: C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stdarg.h
+main.obj: C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stddef.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/knl.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/atm.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/fxn.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/que.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/mem.h
+main.obj: C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stddef.h
+main.obj: C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stdarg.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/sts.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/swi.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/hwi.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/prd.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/sem.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/obj.h
+main.obj: C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stddef.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/sys.h
+main.obj: C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stdarg.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/trg.h
+main.obj: C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stddef.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/_hook.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/gbl.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/tests/biosproj1/Debug/hellocfg.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/hst.h
+main.obj: C:/ti/bios_5_42_01_09/packages/ti/bios/include/pip.h
+main.obj: C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stddef.h
+main.obj: ../ezdsp5502.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/_csl.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_chiphal.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_std.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/tistdtypes.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_stdhal.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_datasz.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_chipdat.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_icachdat.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_irqdat.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_datdat.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_dmadat.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_dmadatB.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_emifdat.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_gpiodat.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_gpio5502dat.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_plldat.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_mcbspdat.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_pwrdat.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_timdat.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_gptdat.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_bctdat.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_errdat.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_mmcdat.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_wdtimdat.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_rtcdat.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_time.h
+main.obj: C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include/linkage.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_i2cdat.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_uartdat.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_csldat.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_cslhal.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_datasz.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_irq.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/_csl.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_irqhal.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_err.h
+main.obj: C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stdint.h
+main.obj: ../aic3204.h
+main.obj: ../ezdsp5502_mcbsp.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_mcbsp.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_mcbsphal.h
+main.obj: ../ezdsp5502_i2cgpio.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_gpio.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_gpio5502.h
+main.obj: Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_gpio5502hal.h
+
+../main.c: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/std.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/tistdtypes.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/log.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/_log.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/trc.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/clk.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/tsk.h: 
+C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stddef.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/knl.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/atm.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/fxn.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/que.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/mem.h: 
+C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stddef.h: 
+C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stdarg.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/sts.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/swi.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/hwi.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/prd.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/sem.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/obj.h: 
+C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stddef.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/sys.h: 
+C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stdarg.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/trg.h: 
+C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stddef.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/_hook.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/gbl.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/tests/biosproj1/Debug/hellocfg.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/hst.h: 
+C:/ti/bios_5_42_01_09/packages/ti/bios/include/pip.h: 
+C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stddef.h: 
+../ezdsp5502.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/_csl.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_chiphal.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_std.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/tistdtypes.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_stdhal.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_datasz.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_chipdat.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_icachdat.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_irqdat.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_datdat.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_dmadat.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_dmadatB.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_emifdat.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_gpiodat.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_gpio5502dat.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_plldat.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_mcbspdat.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_pwrdat.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_timdat.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_gptdat.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_bctdat.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_errdat.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_mmcdat.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_wdtimdat.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_rtcdat.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_time.h: 
+C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include/linkage.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_i2cdat.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_uartdat.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_csldat.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_cslhal.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_datasz.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_irq.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/_csl.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_irqhal.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_err.h: 
+C:/ti/ccsv6/tools/compiler/c5500_4.4.1/include/stdint.h: 
+../aic3204.h: 
+../ezdsp5502_mcbsp.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_mcbsp.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_mcbsphal.h: 
+../ezdsp5502_i2cgpio.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_gpio.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_gpio5502.h: 
+Z:/Documents/Academics/t_ee498/ezdsp5502_v1/C55xxCSL/include/csl_gpio5502hal.h: 
---
+++ programs/BIOS_HWI_IDL/Debug/subdir_vars.mk
@@ -0,0 +1,119 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+TCF_SRCS += \
+../hello.tcf 
+
+S??_SRCS += \
+./hellocfg.s?? 
+
+C_SRCS += \
+../aic3204.c \
+../audioProcessing.c \
+../ezdsp5502.c \
+../ezdsp5502_gpio.c \
+../ezdsp5502_i2c.c \
+../ezdsp5502_i2cgpio.c \
+../ezdsp5502_mcbsp.c \
+./hellocfg_c.c \
+../main.c 
+
+OBJS += \
+./aic3204.obj \
+./audioProcessing.obj \
+./ezdsp5502.obj \
+./ezdsp5502_gpio.obj \
+./ezdsp5502_i2c.obj \
+./ezdsp5502_i2cgpio.obj \
+./ezdsp5502_mcbsp.obj \
+./hellocfg.obj \
+./hellocfg_c.obj \
+./main.obj 
+
+GEN_MISC_FILES += \
+./hello.cdb 
+
+GEN_HDRS += \
+./hellocfg.h \
+./hellocfg.h?? 
+
+S??_DEPS += \
+./hellocfg.d 
+
+C_DEPS += \
+./aic3204.d \
+./audioProcessing.d \
+./ezdsp5502.d \
+./ezdsp5502_gpio.d \
+./ezdsp5502_i2c.d \
+./ezdsp5502_i2cgpio.d \
+./ezdsp5502_mcbsp.d \
+./hellocfg_c.d \
+./main.d 
+
+GEN_CMDS += \
+./hellocfg.cmd 
+
+GEN_FILES += \
+./hellocfg.cmd \
+./hellocfg.s?? \
+./hellocfg_c.c 
+
+GEN_HDRS__QUOTED += \
+"hellocfg.h" \
+"hellocfg.h??" 
+
+GEN_MISC_FILES__QUOTED += \
+"hello.cdb" 
+
+GEN_FILES__QUOTED += \
+"hellocfg.cmd" \
+"hellocfg.s??" \
+"hellocfg_c.c" 
+
+C_DEPS__QUOTED += \
+"aic3204.d" \
+"audioProcessing.d" \
+"ezdsp5502.d" \
+"ezdsp5502_gpio.d" \
+"ezdsp5502_i2c.d" \
+"ezdsp5502_i2cgpio.d" \
+"ezdsp5502_mcbsp.d" \
+"hellocfg_c.d" \
+"main.d" 
+
+S??_DEPS__QUOTED += \
+"hellocfg.d" 
+
+OBJS__QUOTED += \
+"aic3204.obj" \
+"audioProcessing.obj" \
+"ezdsp5502.obj" \
+"ezdsp5502_gpio.obj" \
+"ezdsp5502_i2c.obj" \
+"ezdsp5502_i2cgpio.obj" \
+"ezdsp5502_mcbsp.obj" \
+"hellocfg.obj" \
+"hellocfg_c.obj" \
+"main.obj" 
+
+C_SRCS__QUOTED += \
+"../aic3204.c" \
+"../audioProcessing.c" \
+"../ezdsp5502.c" \
+"../ezdsp5502_gpio.c" \
+"../ezdsp5502_i2c.c" \
+"../ezdsp5502_i2cgpio.c" \
+"../ezdsp5502_mcbsp.c" \
+"./hellocfg_c.c" \
+"../main.c" 
+
+S??_SRCS__QUOTED += \
+"./hellocfg.s??" 
+
+S??_OBJS__QUOTED += \
+"hellocfg.obj" 
+
+
