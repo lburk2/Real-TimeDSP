7a716e378e89b8278717e8248c3c9cd2c268933a
lburk2 <125073389+lburk2@users.noreply.github.com>
2023-05-08 11:22:14 -0500

    Implemented Dave's Ideas
    Went through and cleaned up the code a bit, then implemented dave's suggestions. Project actually works now.

--- programs/AGC/audioProcessing.c
+++ programs/AGC/audioProcessing.c
@@ -13,7 +13,7 @@
 #include "csl_mcbsp.h"
 #include "Dsplib.h"
 
-#define MAX_GAIN 10.0
+#define MAX_GAIN 100.0
 #define MIN_GAIN 0.0
 
 //#include "myNCO.h"
@@ -29,33 +29,34 @@ int16_t rxLeftSample;
 int16_t leftRightFlag = 0;
 int16_t txleftRightFlag = 0;
 
-//int16_t output;
-int16_t outputLP;
-int16_t outputHP;
 int16_t filteredLeftSample[48]={0};
 int16_t msg[48]={0};
 int16_t output[48]={0};
 Int16 filteredLeftSampleOutput;
 
-extern int NCO;
 extern int filterMode;
-extern int16_t* delayLineLPptr;
-extern int16_t* delayLineHPptr;
-extern const int16_t* demoFilterptr;
-extern const int16_t* highPassptr;
-//volatile int indexIn;
 int txcounter=0;
-int windowSize=3;
+
+
+/*
+ * Global Variables for AGC
+ */
+
+#define BUFF_SIZE 200
 float detector=0;
-float detector_array[48]={0};
-int ones[3]= {1,1,1};
-
+float detector_array[BUFF_SIZE]={0};
 float sum_output=0;
-float setpoint=35724.0;
 float gain=1;
+float desiredGain=1;
 float alpha = 1 /  (0.83333333333 * 48000.0);
-float out[48] = {0};
+float out[BUFF_SIZE] = {0};
+float setpoint=14.0;
 
+/*
+ * Tunable variables for AGC
+ */
+float attack = 0.2;
+float decay = 0.001;
 
 
 int start;
@@ -135,53 +136,49 @@ void TSKAudioProcessorFxn(Arg value_arg)
 	{
 		MBX_pend(&MBXAudio, msg, SYS_FOREVER);
 
-		if(!filterMode)
+//		if(!filterMode)
+		if(1)
 		{
-			// Compute Power
+			detector = 0;
+			// find max amplitude
 			for(i=0;i<48;i++)
 			{
-				detector+=((float)msg[i]*(float)msg[i]);
+				if ((float)msg[i]>detector)
+					detector =(float)msg[i];
 			}
-			detector=detector/48.0;
+
 			detector_array[count]= detector;
 			count++;
-			if(count>=48)
+			if(count>=BUFF_SIZE)
 			{
 				count=0;
 			}
 
-			//iir
+			//iir LPF
 			out[0] = alpha * detector_array[0];
-			for( i=1;i<48;i++)
+			for( i=1;i<BUFF_SIZE;i++)
 			{
 			    out[i] = ((1.0-alpha)*out[i-1] + alpha *detector_array[i]);
 			}
 
 			// Average filter output
-			for(i=0;i<48;i++)
+			for(i=0;i<BUFF_SIZE;i++)
 			{
 				sum_output+=out[i];
 			}
-			sum_output=sum_output/48;
+			sum_output=sum_output/200;
 
-		    // Limit gain to allowable range
-		    if (gain > MAX_GAIN) {
-		      gain = MAX_GAIN;
-		    } else if (gain < MIN_GAIN) {
-		      gain = MIN_GAIN;
-		    }
+			//Compute Gain
 
-		    //increase/decrease gain
-			if(sum_output < setpoint)
-			{
-				gain+=0.01;
+			desiredGain = setpoint/sum_output;
+			gain = ((1.0-attack)*desiredGain + attack *gain);
+
+			// Limit gain to allowable range
+			if (gain > MAX_GAIN) {
+			  gain = MAX_GAIN;
+			} else if (gain < MIN_GAIN) {
+			  gain = MIN_GAIN;
 			}
-			else if (sum_output > setpoint)
-			{
-				gain-=0.01;
-			}
-
-//			gain = setpoint/sum_output;
 
 			for(i=0;i<48;i++)
 			{
